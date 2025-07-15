aa4175b20394e4c7837aedaae3db9727b53bef88
khanhlinhoabinh <khanhlinhphamhb@gmail.com>
2025-07-15 14:55:37 +0700

    sua hello

--- src/test/java/com/InfoServletTest.java
+++ src/test/java/com/InfoServletTest.java
@@ -2,7 +2,7 @@ package com;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import com.example.InfoServlet;
 import org.mockito.Mockito;
 import java.io.PrintWriter;
