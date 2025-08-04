7448cd6ab10cb1fec66719a2ef7620e1d91851ac
khanhlinhoabinh <khanhlinhphamhb@gmail.com>
2025-08-05 06:13:14 +0700

    fix

--- src/main/java/com/bookstore/servlet/BookServlet.java
+++ src/main/java/com/bookstore/servlet/BookServlet.java
@@ -30,7 +30,7 @@ public class BookServlet extends HttpServlet {
         // In ra HTML
         PrintWriter out = resp.getWriter();
         out.println("<html><head><title>Sách thiếu nhi</title></head><body>");
-        out.println("<h1>Danh sách sách thiếu nhi</h1>");
+        out.println("<h1>Danh sách sách kid </h1>");
         out.println("<ul>");
         for (Book book : books) {
             out.println("<li>" + book.getTitle() + " - " + book.getAuthor() + " - " + book.getPrice() + "đ</li>");
